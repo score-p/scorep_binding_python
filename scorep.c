920119df2f7c52142c562b62fa8a9bbae2a9e689
Andreas Gocht <andreas.gocht@tu-dresden.de>
2017-05-15 18:13:29 +0200

    add user parameters

--- scorep.c
+++ scorep.c
@@ -48,11 +48,56 @@ static PyObject *region_exit(PyObject *self, PyObject *args)
     return Py_None;
 }
 
+static PyObject *parameter_string(PyObject *self, PyObject *args)
+{
+    const char *name;
+    const char *value;
+
+    if (!PyArg_ParseTuple(args, "ss", &name, &value))
+        return NULL;
+
+    SCOREP_USER_PARAMETER_STRING(name, value)
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *parameter_int(PyObject *self, PyObject *args)
+{
+    const char *name;
+    long long value;
+
+    if (!PyArg_ParseTuple(args, "sL", &name, &value))
+        return NULL;
+
+    SCOREP_USER_PARAMETER_INT64(name, value)
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *parameter_uint(PyObject *self, PyObject *args)
+{
+    const char *name;
+    unsigned long long value;
+
+    if (!PyArg_ParseTuple(args, "sK", &name, &value))
+        return NULL;
+
+    SCOREP_USER_PARAMETER_UINT64(name, value)
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
 static PyMethodDef ScorePMethods[] = {
     {"region_enter", region_enter, METH_VARARGS, "enter a region."},
     {"region_exit", region_exit, METH_VARARGS, "exit a region."},
     {"enable_recording", enable_recording, METH_VARARGS, "disable scorep recording."},
     {"disable_recording", disable_recording, METH_VARARGS, "disable scorep recording."},
+    {"parameter_int", parameter_int, METH_VARARGS, "User parameter int."},
+    {"parameter_uint", parameter_uint, METH_VARARGS, "User parameter uint."},
+    {"parameter_string", parameter_string, METH_VARARGS, "User parameter string."},
     {NULL, NULL, 0, NULL} /* Sentinel */
 };
 

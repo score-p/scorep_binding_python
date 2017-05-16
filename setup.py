3452d3cd91fa1acc6d5dc037fd0e7a5c3585e605
Andreas Gocht <andreas.gocht@tu-dresden.de>
2017-05-16 13:11:21 +0200

    remove print from setup

--- setup.py
+++ setup.py
@@ -26,10 +26,6 @@ libs        = libs.decode("utf-8")
 ldflage     = ldflage.decode("utf-8")
 cflags      = cflags.decode("utf-8")
 
-print(libs)
-print(ldflage)
-print(cflags)
-
 scorep_adapter_init = scorep_adapter_init.decode("utf-8")
 
 lib_dir = re.findall("-L[/+.\w]*",ldflage)

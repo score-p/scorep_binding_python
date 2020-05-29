22e31dc8da2eec523c24bdaaf60527890db762c6
Alexander Grund <Flamefire@users.noreply.github.com>
2020-05-29 13:35:47 +0200

    Use test_ prefix only for actual unit test files to allow the usage of Pytest (#81)
    * Use test_ prefix only for actual unit test files
    
    New developers and some test frameworks (e.g. pytest) search for files
    named 'test_*.py' or '*_test.py' to discover test cases. However
    currently the naming may be confusing as the files named 'test_*.py' contain
    code to be executed by the unittests not the actual unittest itself.
    This also leads to errors in e.g. the Python VSCode extension which
    autodiscovers tests using the above naming scheme,
    
    This renames all test_ files removing the prefix or adding bm_ as the
    prefix for benchmark helpers together with a more descriptive name.
    The main test entrypoint is now test_scorep.py.
    No code changes (except where files were referenced) are done.
    
    * Introduce cases folder for files run by the unit tests

--- test/cases/user_instrumentation.py
+++ test/cases/user_instrumentation.py
@@ -1,11 +1,11 @@
 import scorep
-import test_instrumentation2
+import instrumentation2
 
 
 def foo():
     print("hello world")
-    test_instrumentation2.baz()
-    test_instrumentation2.bar()
+    instrumentation2.baz()
+    instrumentation2.bar()
 
 
 with scorep.instrumenter.enable():

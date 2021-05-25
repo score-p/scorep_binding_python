272f671bbd41c3594a05a82fd3f0e0cee1a14259
NanoNabla <43477372+NanoNabla@users.noreply.github.com>
2021-05-25 16:28:25 +0200

    manual finalize and registering of atexit handler

--- src/methods.cpp
+++ src/methods.cpp
@@ -12,6 +12,9 @@ extern "C"
 
     extern const char* SCOREP_GetExperimentDirName(void);
 
+    extern void SCOREP_RegisterExitHandler(void);
+    extern void SCOREP_FinalizeMeasurement(void);
+
     static PyObject* enable_recording(PyObject* self, PyObject* args)
     {
         SCOREP_User_EnableRecording();
@@ -191,6 +194,18 @@ extern "C"
         return PyUnicode_FromString(scorepy::abspath(path).c_str());
     }
 
+    static PyObject* manual_finalize(PyObject* self, PyObject* args)
+    {
+        SCOREP_FinalizeMeasurement();
+        Py_RETURN_NONE;
+    }
+
+    static PyObject* reregister_exit_handler(PyObject* self, PyObject* args)
+    {
+        SCOREP_RegisterExitHandler();
+        Py_RETURN_NONE;
+    }
+
     static PyMethodDef ScorePMethods[] = {
         { "region_begin", region_begin, METH_VARARGS, "enter a region." },
         { "region_end", region_end, METH_VARARGS, "exit a region." },
@@ -206,6 +221,9 @@ extern "C"
         { "get_experiment_dir_name", get_experiment_dir_name, METH_VARARGS,
           "Get the Score-P experiment dir." },
         { "abspath", abspath, METH_VARARGS, "Estimates the absolute Path." },
+        { "manual_finalize", manual_finalize, METH_VARARGS, "triggers a finalize" },
+        { "reregister_exit_handler", reregister_exit_handler, METH_VARARGS,
+          "register an new atexit handler" },
         { NULL, NULL, 0, NULL } /* Sentinel */
     };
 }

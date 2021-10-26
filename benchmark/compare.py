82067663cf4bf6e5f6632963065076d66e7ff10f
Andreas Gocht <andreas.gocht@tu-dresden.de>
2021-10-26 10:51:24 +0200

    add -s to store

--- benchmark/compare.py
+++ benchmark/compare.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='Compare two benchmarks.',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('left', help='First input for comparison')
 parser.add_argument('right', help='Second input for comparison')
+parser.add_argument('-s', help='short output', action='store_false')
 args = parser.parse_args()
 
 
@@ -45,11 +46,12 @@ for exp in experiment:
             Y_right.append(right_val)
             X.append(numpy.full([len(left_val)], it))
 
-            print("\t\tInterations {}".format(it))
-            print("\t\tMean:     {:>7.4f} s  {:>7.4f} s".format(numpy.mean(left_val), numpy.mean(right_val)))
-            print("\t\tMedian:   {:>7.4f} s  {:>7.4f} s".format(numpy.quantile(left_val, 0.50), numpy.quantile(right_val, 0.50)))
-            print("\t\t5%:       {:>7.4f} s  {:>7.4f} s".format(numpy.quantile(left_val, 0.05), numpy.quantile(right_val, 0.05)))
-            print("\t\t95%:      {:>7.4f} s  {:>7.4f} s".format(numpy.quantile(left_val, 0.95), numpy.quantile(right_val, 0.95)))
+            if args.s:
+                print("\t\tInterations {}".format(it))
+                print("\t\tMean:     {:>7.4f} s  {:>7.4f} s".format(numpy.mean(left_val), numpy.mean(right_val)))
+                print("\t\tMedian:   {:>7.4f} s  {:>7.4f} s".format(numpy.quantile(left_val, 0.50), numpy.quantile(right_val, 0.50)))
+                print("\t\t5%:       {:>7.4f} s  {:>7.4f} s".format(numpy.quantile(left_val, 0.05), numpy.quantile(right_val, 0.05)))
+                print("\t\t95%:      {:>7.4f} s  {:>7.4f} s".format(numpy.quantile(left_val, 0.95), numpy.quantile(right_val, 0.95)))
         Y_left = numpy.asarray(Y_left,dtype=float).flatten()
         Y_right = numpy.asarray(Y_right,dtype=float).flatten()
         X = numpy.asarray(X,dtype=float).flatten()
@@ -57,7 +59,8 @@ for exp in experiment:
         cost_left = linregress(X, Y_left)
         cost_right = linregress(X, Y_right)
 
-        print("")
+        if args.s:
+            print("")
         print("\tSlope     {:>7.4f} ns {:>7.4f} ns".format(cost_left.slope * 1e6, cost_right.slope* 1e6))
         print("\tIntercept {:>7.4f} s  {:>7.4f} s".format(cost_left.intercept, cost_right.intercept))
             
